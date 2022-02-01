4866aae663646fdbf27e59a6d6b06ddea413ab63
mplowey28 <matthew.plowey@gmail.com>
2022-02-01 20:36:35 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  createTheme,
+  Stack,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import Goat from "./assets/images/goat.png";
+import Gold from "./assets/images/goldBars.png";
+import RoundSelect from "./components/RoundSelect";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ThemeProvider theme={darkTheme}>
+        <Stack
+          bgcolor="background.default"
+          textAlign="center"
+          justifyContent="center"
+          height="100vh"
+          width="100%"
         >
-          Learn React
-        </a>
-      </header>
+          <Typography color="text.primary" variant="h3">
+            Welcome to Gold or Goats
+          </Typography>
+          <Stack
+            alignItems="center"
+            direction="row"
+            justifyContent="center"
+            my={2}
+          >
+            <Box
+              component="img"
+              sx={{ height: 98, width: 98 }}
+              src={Gold}
+              alt="gold bars"
+            />
+            <Typography variant="h1"> || </Typography>
+            <Box
+              component="img"
+              sx={{ height: 98, width: 98 }}
+              src={Goat}
+              alt="gold bars"
+            />
+          </Stack>
+          <RoundSelect />
+        </Stack>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/RoundSelect.tsx
@@ -0,0 +1,75 @@
+import { SyntheticEvent, useState } from "react";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+
+const RoundSelect = () => {
+  const [open, setOpen] = useState(false);
+  const [rounds, setRounds] = useState<number>(0);
+
+  const handleChange = (event: SelectChangeEvent<number>) => {
+    setRounds(Number(event.target.value));
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event: SyntheticEvent<unknown>, reason?: string) => {
+    if (reason !== "backdropClick") {
+      setOpen(false);
+    }
+  };
+
+  const possibleRounds = Array.from(Array(101).keys());
+  return (
+    <div>
+      <Button onClick={handleClickOpen} variant="outlined">
+        START A NEW GAME
+      </Button>
+      <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
+        <DialogTitle>How many rounds?</DialogTitle>
+        <DialogContent>
+          <Box
+            component="form"
+            justifyContent="center"
+            sx={{ display: "flex", flexWrap: "wrap" }}
+          >
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel htmlFor="demo-dialog-native">Rounds</InputLabel>
+              <Select
+                native
+                value={rounds}
+                onChange={handleChange}
+                input={<OutlinedInput label="Rounds" id="demo-dialog-native" />}
+              >
+                <option aria-label="None" value="" />
+                {possibleRounds.map((n: number, i: number) => (
+                  <option key={i} value={n}>
+                    {n}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default RoundSelect;

de77b6ffcef098196fcc1c8c134f58f17a9b60e6
mplowey28 <matthew.plowey@gmail.com>
2022-02-05 23:24:24 +0100

    add axios hook

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-
+import useAxios from '../hooks/useAxios'
 import {
   Box,
   Button,
@@ -17,11 +17,25 @@ interface DataProps {
 
 const Results = ({ simParams, switchOpen }: DataProps) => {
   const [resultsOpen, setResultsOpen] = useState<boolean>(false)
+  const [data, setData] = useState<any>()
+  const { response, loading, error } = useAxios({
+    method: 'post',
+    url: 'http://localhost:9090',
+    headers: { 'content-type': 'application/json' },
+    data: JSON.stringify(simParams),
+  })
 
   useEffect(() => {
     simParams.rounds > 0 && !switchOpen && setResultsOpen(true)
   }, [simParams, switchOpen])
 
+  useEffect(() => {
+    if (response !== null) {
+      setData(response)
+    }
+  }, [response])
+
+  console.log(data)
   return (
     <Dialog
       disableEscapeKeyDown
